7259200594e0e20c525197ac7df61e32d717ef76
Aimon Bustardo <aimon.bustardo@dimensiondata.com>
2016-08-14 18:27:25 -0500

    Compliance changes
            -Licensings, layout, RETURN

--- cloud/dimensiondata/dimensiondata_disk.py
+++ cloud/dimensiondata/dimensiondata_disk.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python
-
-from ansible.module_utils.basic import *
-from ansible.module_utils.dimensiondata import *
-import time
-try:
-    from libcloud.common.dimensiondata import DimensionDataAPIException
-    from libcloud.compute.drivers.dimensiondata import DimensionDataNodeDriver
-    import libcloud.security
-    HAS_LIBCLOUD = True
-except:
-    HAS_LIBCLOUD = False
-
-# Get regions early to use in docs etc.
-dd_regions = get_dd_regions()
+# -*- coding: utf-8 -*-
+#
+# (c) 2016 Dimension Data All Rights Reserved.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
 
 DOCUMENTATION = '''
 ---
@@ -40,10 +44,10 @@ options:
       - The target region.
     choices:
       - Regions are defined in Apache libcloud project
-        - file = libcloud/common/dimensiondata.py 
+        - file = libcloud/common/dimensiondata.py
       - See https://libcloud.readthedocs.io/en/latest/
         - ..    compute/drivers/dimensiondata.html
-      - Note that values avail in array dd_regions(). 
+      - Note that values avail in array dd_regions().
       - Note that the default value of na = "North America"
     default: na
   speed:
@@ -126,6 +130,20 @@ servers:
     type: list
     contains: node_ids processed
 '''
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.dimensiondata import *
+import time
+try:
+    from libcloud.common.dimensiondata import DimensionDataAPIException
+    from libcloud.compute.drivers.dimensiondata import DimensionDataNodeDriver
+    import libcloud.security
+    HAS_LIBCLOUD = True
+except:
+    HAS_LIBCLOUD = False
+
+# Get regions early to use in docs etc.
+dd_regions = get_dd_regions()
 
 
 def find_disk_from_scsi_id(node, scsi_id):
